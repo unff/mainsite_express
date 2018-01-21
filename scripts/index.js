1b6024b33758b59a4cf09a3a3094f79b730756e3
JC Ryan <generic82@gmail.com>
2018-01-21 11:06:40 -0500

    added more content

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@ $('#btn_fields').on('click', function() {
     // console.log ('click')
     $('.cover').css('background-image', 'url(../images/field.jpg)')
     $('.cover .content h1').text("Oh look, a field.")
-    $('.cover .content #descriptor').text("Could it be boundless possibilities?")
     setCoverButtons(this.id);
 })
 
@@ -10,7 +9,6 @@ $('#btn_mountains').on('click', function() {
     // console.log ('click')
     $('.cover').css('background-image', 'url(../images/mountain.jpg)')
     $('.cover .content h1').text("Oh look, a mountain.")
-    $('.cover .content #descriptor').text("Could it be majesty?")
     setCoverButtons(this.id);
 })
 
@@ -18,7 +16,6 @@ $('#btn_ponds').on('click', function() {
     // console.log ('click')
     $('.cover').css('background-image', 'url(../images/pond.jpg)')
     $('.cover .content h1').text("Oh look, a pond.")
-    $('.cover .content #descriptor').text("Could it be peace and tranquility?")
     setCoverButtons(this.id);
 })
 
